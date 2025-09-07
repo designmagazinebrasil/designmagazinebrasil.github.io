366f0d13215418d797ca527d1210c0e297262e14
Lucas Fads <lucasfads@gmail.com>
2025-09-07 14:03:21 -0300

    feat: add issues collection and display published editions on the homepage

---
+++ src/content.config.ts
@@ -0,0 +1,17 @@
+import { glob } from "astro/loaders";
+import { z, defineCollection } from "astro:content";
+
+const issuesCollection = defineCollection({
+	loader: glob({ pattern: '**/[^_]*.md', base: './src/data/issues' }),
+	schema: z.object({
+		number: z.number(),
+		year: z.number(),
+		title: z.string(),
+		link: z.string().url().optional(),
+		cover: z.string().optional(),
+	})
+});
+
+export const collections = {
+	"issues": issuesCollection
+};
